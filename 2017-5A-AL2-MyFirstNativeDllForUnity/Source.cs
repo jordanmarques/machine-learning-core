636e82b8111c5618059562163578b13f4820ba87
vchaillou <valentin.chaillou@laposte.net>
2017-04-18 12:46:57 +0200

    First commit
    Work base

---
+++ 2017-5A-AL2-MyFirstNativeDllForUnity/Source.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace MyDll
+{
+    public static class Source
+    {
+        public static int return42()
+        {
+            return 42;
+        }
+    }
+}
+
+
